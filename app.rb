ac706e00a47e07cead5139678726aea3364b7d79
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-19 18:50:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,5 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do 
     enable :sessions 
-    set :session_secret 
+    set :session_secret, 'secret'
+  end 
+  
+  get '/' do 
+    erb :index 
+  end 
+  
+  post '/checkout' do 
+    
 end
